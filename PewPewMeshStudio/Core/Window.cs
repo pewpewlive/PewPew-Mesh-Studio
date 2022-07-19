b22672ca16c9b545d2f2a9e45f34ca2b2123894e
RanPix <darkorden16@gmail.com>
2022-07-19 22:42:51 +0300

    create ui tabs

--- PewPewMeshStudio/Core/Window.cs
+++ PewPewMeshStudio/Core/Window.cs
@@ -3,71 +3,75 @@ using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using PewPewMeshStudio.UI;
 
-namespace PewPewMeshStudio.Core
-{
-    public class Window : GameWindow
-    {   
-        private const int WIDTH = 800;
-        private const int HEIGHT = 600;
+namespace PewPewMeshStudio.Core;
 
-        ImGuiController UIController;
+public class Window : GameWindow
+{   
+    private const int WINDOW_WIDTH = 800;
+    private const int WINDOW_HEIGHT = 600;
 
-        #pragma warning disable CS8618
-        public Window() : base(GameWindowSettings.Default, new NativeWindowSettings()
-        #pragma warning restore CS8618
-        {
-            Size = new Vector2i(WIDTH, HEIGHT),
-            APIVersion = new Version(3, 3),
-            Title = "PewPew Mesh Studio"
-        })
-        { }
+    ImGuiController UIController;
 
-        protected override void OnLoad()
-        {
-            base.OnLoad();
-            Console.WriteLine("Welcome to PewPewMeshStudio!");
-            UIController = new(WIDTH, HEIGHT);
-        }
+    InspectorTAB inspectorTAB = new InspectorTAB();
 
-        protected override void OnResize(ResizeEventArgs Event)
-        {
-            base.OnResize(Event);
-            GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
-            UIController.WindowResized(ClientSize.X, ClientSize.Y);
-        }
+    #pragma warning disable CS8618
+    public Window() : base(GameWindowSettings.Default, new NativeWindowSettings()
+    #pragma warning restore CS8618
+    {
+        Size = new Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT),
+        APIVersion = new Version(3, 3),
+        Title = "PewPew Mesh Studio"
+    })
+    { }
 
-        protected override void OnRenderFrame(FrameEventArgs Event)
-        {
-            base.OnRenderFrame(Event);
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        Console.WriteLine("Welcome to PewPewMeshStudio!");
+        UIController = new(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
 
-            UIController.Update(this, (float)Event.Time);
+    protected override void OnResize(ResizeEventArgs Event)
+    {
+        base.OnResize(Event);
+        GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+        UIController.WindowResized(ClientSize.X, ClientSize.Y);
+    }
 
-            GL.ClearColor(new Color4(0, 32, 90, 235));
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+    protected override void OnRenderFrame(FrameEventArgs Event)
+    {
+        base.OnRenderFrame(Event);
 
-            ImGui.Begin("Testing");
-            if (ImGui.Button("Press me!"))
-                Console.WriteLine("I was pressed!");
-            ImGui.End();
+        UIController.Update(this, (float)Event.Time);
 
-            UIController.Render();
+        GL.ClearColor(new Color4(0, 32, 90, 235));
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
 
-            ImGuiController.CheckGLError("End of frame");
+        ImGui.Begin("Testing");
+        if (ImGui.Button("Press me!"))
+            Console.WriteLine("I was pressed!");
+        ImGui.End();
 
-            SwapBuffers();
-        }
+        inspectorTAB.InitializeTab();
 
-        protected override void OnTextInput(TextInputEventArgs Event)
-        {
-            base.OnTextInput(Event);
-            UIController.PressChar((char)Event.Unicode);
-        }
+        UIController.Render();
 
-        protected override void OnMouseWheel(MouseWheelEventArgs Event)
-        {
-            base.OnMouseWheel(Event);
-            UIController.MouseScroll(Event.Offset);
-        }
+        ImGuiController.CheckGLError("End of frame");
+
+        SwapBuffers();
+    }
+
+    protected override void OnTextInput(TextInputEventArgs Event)
+    {
+        base.OnTextInput(Event);
+        UIController.PressChar((char)Event.Unicode);
+    }
+
+    protected override void OnMouseWheel(MouseWheelEventArgs Event)
+    {
+        base.OnMouseWheel(Event);
+        UIController.MouseScroll(Event.Offset);
     }
 }
---
+++ PewPewMeshStudio/UI/InspectorTAB.cs
@@ -0,0 +1,18 @@
+﻿using ImGuiNET;
+using PewPewMeshStudio.Core;
+
+namespace PewPewMeshStudio.UI;
+
+public class InspectorTAB
+{
+    ImGuiController UIController;
+    
+    public void InitializeTab()
+    {
+        ImGui.Begin("Inspector");
+
+        ImGui.ShowDemoWindow();
+
+        ImGui.End();
+    }
+}
