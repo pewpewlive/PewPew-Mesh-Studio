7bbf9646be66869cbc2b5e20fc2c2e767f0e45c1
SKPG-Tech <42890752+SKPG-Tech@users.noreply.github.com>
2022-07-19 13:38:40 +0300

    Add required packages and example code

--- PewPew-Mesh-Studio/Program.cs
+++ PewPew-Mesh-Studio/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, JF!");
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+Console.WriteLine("Hello, JF!");
+
+using(RenderWindow RendererWindow = new RenderWindow(new VideoMode(1280, 700), "PewPew-Mesh-Studio", Styles.Titlebar | Styles.Close))
+{
+    RendererWindow.SetFramerateLimit(60);
+    RendererWindow.Closed += (s,e) => RendererWindow.Close();
+
+    while (RendererWindow.IsOpen) 
+    {
+        RendererWindow.DispatchEvents();
+        RendererWindow.Clear(new Color(0x1e1e1eff));
+        RendererWindow.Draw(new CircleShape(45.0f));
+        RendererWindow.Display();
+    }
+}
