42a8190eff2a3a6c6f183c4ab42354c829169f23
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2022-07-26 15:31:29 +0300

    Revert "random things"
    This reverts commit 10a18e937f26a55f5812c74e1c3cae84695bcf57.

--- PewPewMeshStudio/ExtraUtils/ConsoleExtension.cs
+++ PewPewMeshStudio/ExtraUtils/ConsoleExtension.cs
@@ -1,22 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
-namespace PewPewMeshStudio.ExtraUtils;
-
-static class ConsoleExtension
+namespace PewPewMeshStudio.ExtraUtils
 {
-    const int SW_HIDE = 0;
-    const int SW_SHOW = 5;
-    readonly static IntPtr handle = GetConsoleWindow();
-    [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
-
-    [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    public static void Hide()
+    static class ConsoleExtension
     {
-        ShowWindow(handle, SW_HIDE); //hide the console
-    }
-    public static void Show()
-    {
-        ShowWindow(handle, SW_SHOW); //show the console
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        readonly static IntPtr handle = GetConsoleWindow();
+        [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static void Hide()
+        {
+            ShowWindow(handle, SW_HIDE); //hide the console
+        }
+        public static void Show()
+        {
+            ShowWindow(handle, SW_SHOW); //show the console
+        }
     }
 }
