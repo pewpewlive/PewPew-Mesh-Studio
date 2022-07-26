5ccf066edb5dd85a2bf149f2125de09b6885f466
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2022-07-26 15:31:29 +0300

    Revert "made axis buttons always be square"
    This reverts commit 5fcf3cba8b080f8be5088198d4476317fe1c1d31.

--- PewPewMeshStudio/UI/Windows/ToolsWindow.cs
+++ PewPewMeshStudio/UI/Windows/ToolsWindow.cs
@@ -13,23 +13,19 @@ public class ToolsWindow
         ImGui.Text("View");
 
         ImGui.PushStyleColor(ImGuiCol.Button, ColorUtil.Vec4ByteToFloat(new Vector4(180, 20, 20, 255)));
-        ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(8f, 3f));
-        ImGui.Button("X");
+        ImGui.Button(" X ");
 
         ImGui.SameLine();
         ImGui.PushStyleColor(ImGuiCol.Button, ColorUtil.Vec4ByteToFloat(new Vector4(20, 180, 20, 255)));
-        ImGui.Button("Y");
+        ImGui.Button(" Y ");
 
         ImGui.SameLine();
         ImGui.PushStyleColor(ImGuiCol.Button, ColorUtil.Vec4ByteToFloat(new Vector4(20, 20, 180, 255)));
-        ImGui.Button("Z");
-
-        ImGui.PopStyleVar();
+        ImGui.Button(" Z ");
 
         ImGui.Checkbox("Isometric", ref perspective);
 
         ImGui.PopStyleColor(3);
-
         ImGui.PopID();
 
         ImGui.End();
