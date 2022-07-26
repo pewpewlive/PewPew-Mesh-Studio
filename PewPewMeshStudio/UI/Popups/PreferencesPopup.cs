1f2727d61400356a128925fa8a2bdca99e20b081
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2022-07-26 15:31:29 +0300

    Revert "add some options and default themes"
    This reverts commit 46fc356baa6d71b429697d24a52652dc8ef3c23e.

--- PewPewMeshStudio/UI/Popups/PreferencesPopup.cs
+++ PewPewMeshStudio/UI/Popups/PreferencesPopup.cs
@@ -7,23 +7,12 @@ namespace PewPewMeshStudio.UI;
 public class PreferencesPopup
 {
     public bool open;
-    public bool antiAliasOn;
-    public bool displayLastAction;
-    // public bool blackBg;
-
-    private String lastActionDone = "Not Applicable";
 
     private string[] prefsItems = { "Project", "Keybinds", "Plugins", "Interface" };
     private int prefSelected;
 
-    private string[] languageItems = { "English", "Lithuanian", "Russian", "Ukrainian", "Greek", "French" };
+    private string[] languageItems = { "English", "Lithuanian", "Russian", "Ukrainian", "Greek" };
     public int langSelected;
-
-    private string[] fontItems = { "Nunito (Default)", "ImGui" };
-    public int fontSelected;
-
-    private string[] themeItems = { "Dark", "Light", "Classic" };
-    public int themeSelected;
 
     public void Initialize(ref bool open1)
     {
@@ -53,19 +42,7 @@ public class PreferencesPopup
         switch (prefSelected)
         {
             case 0:
-                ImGui.Text("Mesh Rendering"); 
-
-                ImGui.Checkbox("Enable Antialiasing", ref antiAliasOn);
-                if (ImGui.IsItemHovered())
-                    ImGui.SetTooltip("Toggles mesh antialiasing in Editor.");
-
-                /*
-                ImGui.Checkbox("Display Latest Action", ref displayLastAction);
-                if (ImGui.IsItemHovered())
-                    ImGui.SetTooltip("Displays last action you've done at the bottom of the Editor window.");
-                */
-
-                //ImGui.Checkbox("Toggle Black Background", ref blackBg); 
+                ImGui.Text("You can do here something lol");
                 return;
 
             case 1:
@@ -77,49 +54,16 @@ public class PreferencesPopup
                 return;
 
             case 3:
-                // Multiple ListBoxes in the same case require different labels. Remember that and put multiple spaces.
                 ImGui.Text("Language");
                 ImGui.ListBox(" ", ref langSelected, languageItems, languageItems.Length);
 
                 ImGui.NewLine();
 
                 ImGui.Text("Font");
-       
-                ImGui.ListBox("  ", ref fontSelected, fontItems, fontItems.Length);
-
-                ImGui.NewLine();
-
-                ImGui.Text("Theme");
-                ImGui.ListBox("   ", ref themeSelected, themeItems, themeItems.Length);
-                ChangeTheme(themeSelected);
-
                 return;
 
             default: // if some shit happens
                 Console.WriteLine("Invalid preference list item index");
-                return;
-        }
-
-    }
-
-    private void ChangeTheme(int themeId)
-    {
-        switch (themeId)
-        {
-            case 0:
-                ImGui.StyleColorsDark();
-                return;
-
-            case 1:
-                ImGui.StyleColorsLight();
-                return;
-
-            case 2:
-                ImGui.StyleColorsClassic();
-                return;
-
-            default:
-                ImGui.StyleColorsDark();
                 return;
         }
     }
