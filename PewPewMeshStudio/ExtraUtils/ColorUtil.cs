641e00780080401d08c1d7a64bc2d8592f3ea72a
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2022-07-26 15:31:29 +0300

    Revert "rename method name"
    This reverts commit 3babf88f946c85a7ec02073405ecd5551ecd8b83.

--- PewPewMeshStudio/ExtraUtils/ColorUtil.cs
+++ PewPewMeshStudio/ExtraUtils/ColorUtil.cs
@@ -4,7 +4,7 @@ namespace PewPewMeshStudio.UI
 {
     public class ColorUtil
     {
-        public static Vector4 Vec4ByteToFloat(Vector4 Vec4Int)
+        public static Vector4 Vec4IntToFloat(Vector4 Vec4Int)
         {
             return new Vector4(Vec4Int.X / 255.0f, Vec4Int.Y / 255.0f, Vec4Int.Z / 255.0f, Vec4Int.W / 255.0f);
         }
--- PewPewMeshStudio/UI/Popups/UnsavedChangesPopup.cs
+++ PewPewMeshStudio/UI/Popups/UnsavedChangesPopup.cs
@@ -14,14 +14,14 @@ public class UnsavedChangesPopup
         if (!ImGui.BeginPopupModal("Alert", ref open))
             return;
 
-        ImGui.TextColored(ColorUtil.Vec4ByteToFloat(new Vector4(225, 50, 50, 255)), "You have unsaved changes\nYou sure you want to quit?");
+        ImGui.TextColored(ColorUtil.Vec4IntToFloat(new Vector4(225, 50, 50, 255)), "You have unsaved changes\nYou sure you want to quit?");
         ImGui.Checkbox("Don't show this again", ref dontShowThisAgain);
 
         ImGui.Separator();
 
-        ImGui.PushStyleColor(ImGuiCol.Button, ColorUtil.Vec4ByteToFloat(new Vector4(200, 50, 50, 255)));
-        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, ColorUtil.Vec4ByteToFloat(new Vector4(150, 50, 50, 255)));
-        ImGui.PushStyleColor(ImGuiCol.ButtonActive, ColorUtil.Vec4ByteToFloat(new Vector4(100, 50, 50, 255)));
+        ImGui.PushStyleColor(ImGuiCol.Button, ColorUtil.Vec4IntToFloat(new Vector4(200, 50, 50, 255)));
+        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, ColorUtil.Vec4IntToFloat(new Vector4(150, 50, 50, 255)));
+        ImGui.PushStyleColor(ImGuiCol.ButtonActive, ColorUtil.Vec4IntToFloat(new Vector4(100, 50, 50, 255)));
         ImGui.Button("Quit");
         ImGui.PopStyleColor(3);
 
